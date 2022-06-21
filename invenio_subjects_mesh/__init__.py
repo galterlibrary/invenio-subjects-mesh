48031819e70ee3768a3eb716b2ad7c9160522970
Guillaume Viger <fenekku@fenekku.com>
2022-06-21 15:55:47 -0400

    release: 2022.06.21

--- invenio_subjects_mesh/__init__.py
+++ invenio_subjects_mesh/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2021 Northwestern University.
+# Copyright (C) 2021-2022 Northwestern University.
 #
 # invenio-subjects-mesh is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -8,6 +8,5 @@
 
 """MeSH subject terms for InvenioRDM."""
 
-from .version import __version__
 
-__all__ = ('__version__')
+__version__ = '2022.06.21'
